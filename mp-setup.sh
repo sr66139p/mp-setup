c842c87dc536f699418013646a8e07ff87bacfba
sr66139p <144626133+sr66139p@users.noreply.github.com>
2025-07-17 15:09:42 -0400

    Update mp-setup.sh to install future package

--- mp-setup.sh
+++ mp-setup.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get purge -y modemmanager
 sudo apt-get install -y python3-dev python3-opencv python3-pip python3-matplotlib python3-lxml python3-pygame
-python3 -m pip install PyYAML mavproxy --user --break-system-packages
+python3 -m pip install PyYAML future mavproxy --user --break-system-packages
 echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
